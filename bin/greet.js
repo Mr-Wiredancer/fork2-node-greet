46dce1b21a5238d500d75df361c98f9718874cf7
Mr. Wiredancer <jiahao@dxhackers.com>
2014-03-24 18:09:30 +0800

    greet-drunk

--- bin/greet.js
+++ bin/greet.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
-var greet = require( "../index.js" );
+var greet = require( "../index.js" )
+    , parseArgs = require('minimist');
 
-greet( process.argv[2] );
+var argv = parseArgs( process.argv.slice(2) );
+
+console.log(greet( argv['_'][0], "drunk" in argv ));
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = function(name){return console.log("hello, "+name)};
+module.exports = function( name, drunk ){
+    if (drunk) {
+        return "hello " + name + ", you look sexy today";
+    } else {
+        return "hello, " + name;    
+    }
+};
