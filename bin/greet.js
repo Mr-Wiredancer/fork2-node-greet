453077c2949caf82c6efde21296ab2f2fffea627
Mr. Wiredancer <jiahao@dxhackers.com>
2014-03-24 18:37:02 +0800

    trivial joke

--- bin/greet.js
+++ bin/greet.js
@@ -5,4 +5,8 @@ var greet = require( "../index.js" )
 
 var argv = parseArgs( process.argv.slice(2) );
 
-console.log(greet( argv['_'][0], "drunk" in argv ));
+if ( argv['_'].length > 0 ){
+    console.log( greet( argv['_'][0], "drunk" in argv ));
+}else{
+    console.log( "不要坑我啦，至少给个名字嘛(抠鼻)" );   
+}
